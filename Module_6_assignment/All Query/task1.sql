b1e5cb91a95e4329e84b437407b0547789620006
devmokit <149257009+devmokit@users.noreply.github.com>
2023-11-06 19:01:59 +0600

    Delete Module_6_assignment directory

--- Module_6_assignment/All Query/task1.sql
+++
@@ -1,5 +0,0 @@
-SELECT c.id AS customer_id, c.name, c.email, c.location, COUNT(orders.id) AS num_orders
-FROM customers c
-LEFT JOIN orders ON c.id = orders.customer_id
-GROUP BY c.id
-ORDER BY num_orders DESC;
--- Module_6_assignment/All Query/task3.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.name AS category_name, SUM(oi.unit_price * oi.quantity) AS total_revenue
-FROM order_items oi
-INNER JOIN products p ON oi.product_id = p.id
-INNER JOIN categories c ON p.category_id = c.id
-GROUP BY c.id
-ORDER BY total_revenue DESC;
